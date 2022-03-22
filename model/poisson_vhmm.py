0d4bb0756b82e2559c2f6253740e27d6448fbf47
morim <acetaminophen151@gmail.com>
2022-03-22 17:41:19 +0900

    complete implement poisson vhmm

--- model/poisson_vhmm.py
+++ model/poisson_vhmm.py
@@ -3,6 +3,7 @@ from jax.scipy.special import digamma, gammaln
 import jax.numpy as jnp
 
 from model.vhmm_base import VHMMBase
+import numpy as np
 
 
 class PoissonVHMM(VHMMBase):
@@ -11,10 +12,10 @@ class PoissonVHMM(VHMMBase):
                  iter_max=100, converge_threshold=1e-3):
         super().__init__(init_state_prior, transition_prior)
         self.state_num = state_num
-        self.poisson_prior_a = poisson_prior[0]
-        self.poisson_prior_b = poisson_prior[1]
-        self.poisson_posterior_a = jnp.repeat(poisson_prior[0], state_num)
-        self.poisson_posterior_b = jnp.repeat(poisson_prior[1], state_num)
+        self.poisson_prior_a = jnp.array(poisson_prior[0])
+        self.poisson_prior_b = jnp.array(poisson_prior[1])
+        self.poisson_posterior_a = jnp.array(np.repeat(poisson_prior[0], state_num) + np.random.rand(state_num) * 1.)
+        self.poisson_posterior_b = jnp.array(np.repeat(poisson_prior[1], state_num) + np.random.rand(state_num) * 1.)
 
         self.iter_max = iter_max
         self.converge_threshold = 1e-3
@@ -22,9 +23,10 @@ class PoissonVHMM(VHMMBase):
     @staticmethod
     @jit
     def _obs_log_prob(obs, poisson_posterior_a, poisson_posterior_b):
-        term1 = obs * jnp.expand_dims(digamma(poisson_posterior_a) - jnp.log(poisson_posterior_b), axis=(0, 1))
+        term1 = obs[..., jnp.newaxis] \
+                * (digamma(poisson_posterior_a) - jnp.log(poisson_posterior_b))[jnp.newaxis, jnp.newaxis]
         term2 = - (poisson_posterior_a / poisson_posterior_b)[jnp.newaxis, jnp.newaxis]
-        term3 = - gammaln(obs)
+        term3 = - gammaln(obs)[..., jnp.newaxis]
         return term1 + term2 + term3
 
     def obs_log_prob(self, obs):
@@ -33,25 +35,23 @@ class PoissonVHMM(VHMMBase):
     def maximize_observations(self, obs, gamma):
         """
 
-        :param obs: jnp array, shape (time, batch, hidden)
+        :param obs: jnp array, shape (time, batch)
         :param gamma: jnp array, shape (time, batch, hidden)
         :return:
         """
-        self.poisson_posterior_a = jnp.sum(obs * gamma, axis=(0, 1)) + self.poisson_prior_a
+        self.poisson_posterior_a = jnp.sum(obs[..., jnp.newaxis] * gamma, axis=(0, 1)) + self.poisson_prior_a
         self.poisson_posterior_b = jnp.sum(gamma, axis=(0, 1)) + self.poisson_prior_b
 
     def fit(self, obs):
 
         for i in range(self.iter_max):
             gamma, xi = self.e_step(obs)
-            print(xi)
             self.maximize_transitions(gamma, xi)
             self.maximize_observations(obs, gamma)
             elbo = self.elbo(obs)
             print(elbo)
 
-        return gamma, self.viterbi(obs)
-
+        return gamma, self.viterbi(obs), elbo
 
     @staticmethod
     def _kl_lambda(q_a, q_b, p_a, p_b):
@@ -69,6 +69,7 @@ class PoissonVHMM(VHMMBase):
 
     def elbo(self, obs):
         gamma, xi = self.e_step(obs)
+
         return (jnp.sum(self.obs_log_prob(obs) * gamma)
                 - self._kl_lambda(self.poisson_posterior_a,
                                   self.poisson_posterior_b,
--- model/vhmm_base.py
+++ model/vhmm_base.py
@@ -4,7 +4,7 @@ import jax.numpy as jnp
 from jax.scipy.special import logsumexp
 from jax import lax, jit
 from jax.lax import lgamma
-from jax.scipy.special import digamma, xlogx
+from jax.scipy.special import digamma, xlogy
 
 
 class HMMBase:
@@ -43,59 +43,55 @@ class HMMBase:
         log_prob = jnp.expand_dims(alpha, axis=2) + jnp.expand_dims(trans_log_prob, axis=0)
 
         result = logsumexp(log_prob, axis=1) + obs_log_prob
-        cond_log_likelihood = logsumexp(result, axis=1)
-        return result, cond_log_likelihood
+        cond_log_likelihood = logsumexp(result, axis=-1, keepdims=True)
+        return result - cond_log_likelihood
 
     @staticmethod
     @jit
-    def _backward_one_step(beta, trans_log_prob, obs_log_prob, cond_log_likelihood):
+    def _backward_one_step(beta, trans_log_prob, obs_log_prob, ):
         '''
 
         :param beta: jnp array, shape (batch_size, hidden_num)
         :param trans_log_prob: jnp array, shape (hidden, hidden)
         :param obs_log_prob: jnp array, shape (batch, hidden)
-        :params cond_log_likelihood: jnp array, shape (batch)
         :return:
         '''
         log_prob = jnp.expand_dims(beta + obs_log_prob, axis=1) + jnp.expand_dims(trans_log_prob, axis=0)
 
-        result = logsumexp(log_prob, axis=2) - jnp.expand_dims(cond_log_likelihood, axis=-1)
+        result = logsumexp(log_prob, axis=2)
 
-        return result
+        return result - logsumexp(result, axis=-1, keepdims=True)
 
     @staticmethod
     @jit
     def forward_step(init_log_prob, obs_log_probs, trans_log_prob):
 
         def scan_fn(log_prob, obs_log_prob):
-            result, partition = HMMBase._forward_one_step(log_prob, trans_log_prob,
-                                                          obs_log_prob)
-            normed_result = result - jnp.expand_dims(partition, axis=-1)
+            result = HMMBase._forward_one_step(log_prob, trans_log_prob,
+                                               obs_log_prob)
             return (
-                normed_result,
-                [normed_result, partition]
+                result,
+                result
             )
 
-        _, [forward_log_prob, cond_log_likelihood] = lax.scan(scan_fn, init_log_prob, obs_log_probs)
-        return forward_log_prob, cond_log_likelihood
+        _, forward_log_prob = lax.scan(scan_fn, init_log_prob, obs_log_probs)
+        return forward_log_prob
 
     @staticmethod
     @jit
-    def backward_step(obs_log_probs, trans_log_prob, cond_log_likelihoods):
+    def backward_step(obs_log_probs, trans_log_prob):
 
         @jit
-        def scan_fn(beta, params):
-            obs_log_prob = params[0]
-            cond_log_likelihood = params[1]
+        def scan_fn(beta, obs_log_prob):
             result = HMMBase._backward_one_step(beta, trans_log_prob,
-                                                obs_log_prob, cond_log_likelihood)
+                                                obs_log_prob)
             return (
                 result,
                 result
             )
 
         _, backward_log_prob = lax.scan(scan_fn, jnp.zeros_like(obs_log_probs[0]),
-                                        [obs_log_probs, cond_log_likelihoods],
+                                        obs_log_probs,
                                         reverse=True)
         return backward_log_prob
 
@@ -107,39 +103,41 @@ class HMMBase:
         """
 
         initial_forward = obs_log_probs[0] + jnp.expand_dims(initial_log_prob, axis=0)
-        initial_log_likelihood = logsumexp(initial_forward, axis=-1)
-        initial_forward = initial_forward - jnp.expand_dims(initial_log_likelihood, axis=1)
+        initial_log_likelihood = logsumexp(initial_forward, axis=-1, keepdims=True)
+        initial_forward = initial_forward - initial_log_likelihood
 
-        forward_log_probs, cond_log_likelihoods = HMMBase.forward_step(initial_forward, obs_log_probs[1:],
-                                                                       trans_log_prob)
+        forward_log_probs = HMMBase.forward_step(initial_forward, obs_log_probs[1:], trans_log_prob)
         forward_log_probs = jnp.concatenate([initial_forward[jnp.newaxis], forward_log_probs])
-        cond_log_likelihoods = jnp.concatenate([initial_log_likelihood[jnp.newaxis], cond_log_likelihoods])
 
-        backward_log_probs = HMMBase.backward_step(obs_log_probs[1:], trans_log_prob, cond_log_likelihoods[1:])
+        backward_log_probs = HMMBase.backward_step(obs_log_probs[1:], trans_log_prob)
         backward_log_probs = jnp.concatenate([backward_log_probs, jnp.zeros_like(obs_log_probs[0])[jnp.newaxis]])
 
-        return forward_log_probs, backward_log_probs, cond_log_likelihoods
+        return forward_log_probs, backward_log_probs
 
     @staticmethod
     @jit
     def _calc_gamma(forward, backward):
-        return jnp.exp(forward + backward)
+        not_normed = forward + backward
+        normed = not_normed - logsumexp(not_normed, axis=-1, keepdims=True)
+        return jnp.exp(normed)
 
     @staticmethod
     @jit
-    def _calc_xi(forward, backward, scale, trans_log_prob, obs_log_probs):
-        return jnp.exp(forward[:-1][..., jnp.newaxis] + jnp.expand_dims(trans_log_prob, axis=(0, 1)) \
-                       + (obs_log_probs[1:] + backward[1:] + scale[1:][..., jnp.newaxis])[..., jnp.newaxis, :])
+    def _calc_xi(forward, backward, trans_log_prob, obs_log_probs):
+        not_normed = forward[:-1][..., jnp.newaxis] + jnp.expand_dims(trans_log_prob, axis=(0, 1)) \
+                   + (obs_log_probs[1:] + backward[1:])[..., jnp.newaxis, :]
+        normed = not_normed - logsumexp(not_normed, axis=-1, keepdims=True)
+        return jnp.exp(normed)
 
     def e_step(self, obs):
         obs_log_probs = self.obs_log_prob(obs)
         trans_log_prob = self.trans_log_prob()
         initial_log_prob = self.initial_log_prob()
 
-        forward, backward, scale = HMMBase._e_step(obs_log_probs, initial_log_prob, trans_log_prob, )
+        forward, backward = HMMBase._e_step(obs_log_probs, initial_log_prob, trans_log_prob, )
 
         gamma = HMMBase._calc_gamma(forward, backward)
-        xi = HMMBase._calc_xi(forward, backward, scale, trans_log_prob, obs_log_probs)
+        xi = HMMBase._calc_xi(forward, backward, trans_log_prob, obs_log_probs)
         return gamma, xi
 
     @staticmethod
@@ -170,7 +168,7 @@ class HMMBase:
         elem = jnp.concatenate([obs_plus_trans, jnp.zeros_like(obs_plus_trans[0])[jnp.newaxis]], axis=0)
         backward = lax.associative_scan(HMMBase._viterbi_one_step, elem, reverse=True)
 
-        result = jnp.argmax(forward[:, :, 0] + backward[:, :, :, 0], axis=2)
+        result = jnp.argmax(forward[:, :, 0] + backward[:, :, 0], axis=2)
         return result
 
     def viterbi(self, obs):
@@ -197,7 +195,7 @@ class VHMMBase(HMMBase):
 
     def trans_log_prob(self):
         return digamma(self.transition_posterior) \
-               - digamma(jnp.sum(self.transition_posterior, axis=1))[:, jnp.newaxis]
+               - digamma(jnp.sum(self.transition_posterior, axis=1))[..., jnp.newaxis]
 
     def initial_log_prob(self):
         return digamma(self.init_state_posterior) - digamma(jnp.sum(self.init_state_posterior))
@@ -230,10 +228,10 @@ class VHMMBase(HMMBase):
         """
 
         :param q: (..., category_num)
-        :param p: (..., category_num)
+        :param log_p: (..., category_num)
         :return:
         """
-        return jnp.sum(xlogx(q) - q * log_p)
+        return jnp.sum(xlogy(q, q) - q * log_p)
 
     def _kl_initial_state(self):
         return VHMMBase._kl_dirichlet_dirichlet(self.init_state_posterior, self.init_state_prior)
@@ -251,11 +249,9 @@ class VHMMBase(HMMBase):
         """
         log_p = self.initial_log_prob()
         q = gamma[0]
-        print("q", q, "log_p", log_p)
         term1 = VHMMBase._kl_categorical(q, log_p)
 
         log_p = self.trans_log_prob()
         q = xi
-        print("A", "q", q, "log_p", log_p)
         term2 = VHMMBase._kl_categorical(q, log_p)
         return term1 + term2
