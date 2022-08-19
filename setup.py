da9c87dea6e6f8e9643662798cda46ad49448296
Kohei Morimoto <48996660+morim3@users.noreply.github.com>
2022-08-20 00:38:10 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+setup(
+    name="VariationalHMM",
+    version="0.1.0",
+    description="Jax-Implementation of Variational Hidden Markov Model",
+    author="morim3",
+    url="https://github.com/morim3/VariationalHMM",
+    packages=find_packages("model"),
+    package_dir={"": "model"},
+    py_modules=[splitext(basename(path))[0] for path in glob('model/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=_requires_from_file('requirements.txt'),
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-cov"]
+)
